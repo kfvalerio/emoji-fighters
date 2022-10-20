f2f1bd3f3283cda5fe631ff2817dd262ca3a7ec1
KF Valerio <kffvalerio@gmail.com>
2022-10-20 11:44:18 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+  let randomIndex1 = Math.floor(Math.random() * fighters.length)
+  let randomIndex2 = Math.floor(Math.random() * fighters.length)
+  stageEl.textContent = fighters[randomIndex1] + " vs " + fighters[randomIndex2]
+})
